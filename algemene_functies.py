99ad7e849db5069b856349243d6b292f14da26ec
Corine Macleane <corinemacleane@gmail.com>
2026-03-31 14:57:50 +0200

    een aantal algemene functies/berekeningen toevoegen

---
+++ algemene_functies.py
@@ -0,0 +1,7 @@
+# Opgave 2: functie die kwadraat van getal teruggeeft
+def mijn_functie_1(argument):
+    return argument ** 2
+
+# Opgave 3: functie die berekeningen uitvoert en teruggeeft als lijst
+def mijn_functie_2(getal_1, getal_2):
+    return [getal_1 + getal_2, getal_1 - getal_2, getal_1 * getal_2, round(getal_1 / getal_2)]
